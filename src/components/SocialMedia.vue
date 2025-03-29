212483b76a030436f2a4521fec07a9e0c9b1aebf
Haraldur Ingi Haraldsson <haralduringiha@gmail.com>
2025-03-29 16:26:14 +0000

    social media youtube

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -15,17 +15,29 @@
       </a>
     </div>
 
-    <div class="w-full flex items-center justify-center">
-      <div class="w-2/3 shadow-md">
+    <div class="w-full flex items-center justify-center flex-col gap-6">
+      <div class="w-full flex justify-center">
         <iframe
           style="border-radius: 12px"
           src="https://open.spotify.com/embed/artist/6AVM9rg36bwti89n0X7qNw?utm_source=generator&theme=0"
-          width="100%"
-          height="352"
+          class="w-full md:w-[560px] h-[352px] shadow-md"
           frameBorder="0"
           allowfullscreen=""
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
+        ></iframe>
+      </div>
+      <div class="w-full flex justify-center rounded-lg overflow-hidden">
+        <iframe
+          width="560"
+          height="315"
+          class="w-full md:w-[560px] h-[352px] shadow-md"
+          src="https://www.youtube.com/embed/RkT_ASKoi9A?si=akxiIMm-2LiDmS_6"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
         ></iframe>
       </div>
     </div>
